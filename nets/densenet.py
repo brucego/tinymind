0cb0eed4d14c23b152bd66dc9b304d0684fe801d
Chensheng <isunrise@qq.com>
2018-07-17 22:30:33 +0800

    good_6

--- nets/densenet.py
+++ nets/densenet.py
@@ -62,18 +62,18 @@ def densenet(images, num_classes=1001, is_training=False,
             end_point = 'Conv2d_1a_3x3'
             ##########################
             # Put your code here.
-            net = block(images, layers = 4, growth=8, scope=end_point)
+            net = block(images, layers = 4, growth=4, scope=end_point)
             end_points[end_point] = net
 
             end_point = 'Conv2d_2a_3x3'
-            net = block(images, layers = 4, growth=8, scope=end_point)
+            net = block(images, layers = 4, growth=4, scope=end_point)
             end_points[end_point] = net
 			
             end_point = 'Conv2d_3a_3x3'
-            net = block(images, layers = 4, growth=8, scope=end_point)
+            net = block(images, layers = 4, growth=4, scope=end_point)
             end_points[end_point] = net
 			
-            logits = slim.conv2d(net, num_classes, [224, 224], activation_fn=None,
+            logits = slim.conv2d(net, num_classes, [1, 1], activation_fn=None,
                              normalizer_fn=None, scope='Conv2d_o_1x1')
             #logits = tf.squeeze(logits, [1, 2], name='SpatialSqueeze')
             ##########################
