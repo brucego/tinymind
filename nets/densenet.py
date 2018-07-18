77ec199968841d379e75a397260dc00a14b474ff
Chensheng <isunrise@qq.com>
2018-07-18 10:13:31 +0800

    good_8

--- nets/densenet.py
+++ nets/densenet.py
@@ -87,6 +87,7 @@ def densenet(images, num_classes=1001, is_training=False,
             net = slim.conv2d(net, 32, [3, 3], scope=scope + '_conv_pool_5')
             net = slim.max_pool2d(net, [3, 3], stride=2, scope=end_point)
             net = slim.max_pool2d(net, [2, 2], stride=2, scope=end_point)
+            net = tf.reshape(net, [32,200])
 
             logits = slim.conv2d(net, num_classes, [1, 1], activation_fn=None,
                              normalizer_fn=None, scope='Conv2d_o_1x1')
